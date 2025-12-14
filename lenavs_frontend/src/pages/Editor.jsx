50df03197facf39cb81996b11e96d1db54d30d34
lleonards <lleonards.777@gmail.com>
2025-12-14 12:35:54 -0300

    Update Editor.jsx

--- lenavs_frontend/src/pages/Editor.jsx
+++ lenavs_frontend/src/pages/Editor.jsx
@@ -1,39 +1,50 @@
-import React, { useState } from 'react';
-import Header from '../components/Editor/Header';
-import UploadPanel from '../components/Editor/UploadPanel';
-import PreviewPanel from '../components/Editor/PreviewPanel';
-import LyricsEditor from '../components/Editor/LyricsEditor';
-import ExportPanel from '../components/Editor/ExportPanel';
-import HelpModal from '../components/Modals/HelpModal';
-import ProjectsModal from '../components/Modals/ProjectsModal';
-import LibraryModal from '../components/Modals/LibraryModal';
-import { useEditorStore } from '../store/editorStore';
-import './Editor.css';
+import React, { useState } from 'react'
+import Header from '../components/Editor/Header'
+import UploadPanel from '../components/Editor/UploadPanel'
+import PreviewPanel from '../components/Editor/PreviewPanel'
+import LyricsEditor from '../components/Editor/LyricsEditor'
+import ExportPanel from '../components/Editor/ExportPanel'
+import HelpModal from '../components/Modals/HelpModal'
+import ProjectsModal from '../components/Modals/ProjectsModal'
+import LibraryModal from '../components/Modals/LibraryModal'
+import TrialExpired from '../components/TrialExpired'
+import { useAuth } from '../context/AuthContext'
+import { useEditorStore } from '../store/editorStore'
+import './Editor.css'
 
 function Editor() {
-  const hasLyrics = useEditorStore(state => state.hasLyrics);
-  const [showHelp, setShowHelp] = useState(false);
-  const [showProjects, setShowProjects] = useState(false);
-  const [showLibrary, setShowLibrary] = useState(false);
+  const { trialExpired } = useAuth()
+  const hasLyrics = useEditorStore(state => state.hasLyrics)
+
+  const [showHelp, setShowHelp] = useState(false)
+  const [showProjects, setShowProjects] = useState(false)
+  const [showLibrary, setShowLibrary] = useState(false)
+
+  // -----------------------------
+  // BLOCK EDITOR IF TRIAL EXPIRED
+  // -----------------------------
+  if (trialExpired) {
+    return <TrialExpired />
+  }
 
   return (
     <div className="editor-container">
-      <Header 
+      <Header
         onHelp={() => setShowHelp(true)}
         onProjects={() => setShowProjects(true)}
         onLibrary={() => setShowLibrary(true)}
       />
-      
+
       <div className="editor-content">
         <div className="editor-left">
           <UploadPanel />
         </div>
-        
+
         <div className="editor-center">
           <PreviewPanel />
           {hasLyrics && <LyricsEditor />}
         </div>
-        
+
         <div className="editor-right">
           <ExportPanel />
         </div>
@@ -43,7 +54,7 @@ function Editor() {
       {showProjects && <ProjectsModal onClose={() => setShowProjects(false)} />}
       {showLibrary && <LibraryModal onClose={() => setShowLibrary(false)} />}
     </div>
-  );
+  )
 }
 
-export default Editor;
+export default Editor
