7725533626a49365c65bd0c8e420fcff3e0b8500
lleonards <lleonards.777@gmail.com>
2025-12-19 23:01:14 -0300

    Update authStore.js

--- lenavs_frontend/src/store/authStore.js
+++ lenavs_frontend/src/store/authStore.js
@@ -1,27 +1,21 @@
 import { create } from 'zustand';
-import { persist } from 'zustand/middleware';
 
-export const useAuthStore = create(
-  persist(
-    (set) => ({
+export const useAuthStore = create((set) => ({
+  user: null,
+  token: null,
+  isAuthenticated: false,
+
+  setAuth: (user, token) =>
+    set({
+      user,
+      token,
+      isAuthenticated: true
+    }),
+
+  logout: () =>
+    set({
       user: null,
       token: null,
-      isAuthenticated: false,
-      
-      setAuth: (user, token) => set({
-        user,
-        token,
-        isAuthenticated: true
-      }),
-      
-      logout: () => set({
-        user: null,
-        token: null,
-        isAuthenticated: false
-      })
-    }),
-    {
-      name: 'lenavs-auth'
-    }
-  )
-);
+      isAuthenticated: false
+    })
+}));
